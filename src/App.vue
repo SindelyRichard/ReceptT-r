bd6b8268e2d744d92dcd4f583bebf2b13bfb4730
Richard Sindely <rsindely@gmail.com>
2025-01-10 16:45:17 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <nav class="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <div class="welcome">
+      <HelloWorld/>
+    </div>
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/mainPage.vue'
 
 export default {
   name: 'App',
@@ -15,12 +27,42 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+}
+.navbar {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: rgb(73, 73, 73);
+  color: white;
+  padding: 10px 20px;
+}
+
+.navbar ul {
+  list-style: none;
+  display: flex;
+  gap: 20px;
+}
+
+.navbar ul li {
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.navbar ul li {
+  text-decoration: underline;
 }
 </style>
---
+++ src/components/mainPage.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="app">
+    <div class="welcome">Üdvözöljük a RecepTár oldalon!</div>
+    <div class="trapezoid">
+      <img :src="currentImage" alt="Dynamic image" class="image" />
+    </div>
+    <div class="details">Változatos és különféle receptek csak önnek!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        require('@/assets/food1.jpg'),
+        require('@/assets/food4.jpg'),
+        require('@/assets/food3.jpg'),
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    currentImage() {
+      return this.images[this.currentIndex];
+    },
+  },
+  mounted() {
+    this.startImageRotation();
+  },
+  methods: {
+    startImageRotation() {
+      setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      }, 3000);
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.app {
+  display: flex;
+  justify-content: center;
+  background-image: url("../assets/panel.jpeg");
+  width: 100vw;
+}
+
+.trapezoid {
+  width: 800px;
+  height: 200px;
+  clip-path: polygon(10% 0%, 90% 0%, 100% 100%, 0% 100%);
+  background: #000;
+  position: relative;
+  overflow: hidden;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.welcome{
+   position: absolute;
+   left: 10%;
+   top: 13%;
+   color: white;
+   font-family: 'Courier New', Courier, monospace;
+   font-weight: bold;
+}
+.details{
+  position: absolute;
+  right: 7%;
+  top: 13%;
+  color: white;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+}
+</style>
